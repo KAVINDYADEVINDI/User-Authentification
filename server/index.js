82c26a34b09966bcc2bb29a9c254fd5c63b5986f
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-05-18 23:58:53 -0700

    new

--- server/index.js
+++ server/index.js
@@ -2,5 +2,10 @@ const express = require('express');
 const mongoose=require('mongoose');
 const app = express();
 
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/usersAuth',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
 app.listen(3001, () => { console.log("server is running") })
-mongoose.connect()
